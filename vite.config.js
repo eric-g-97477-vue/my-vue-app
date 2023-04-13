563f4275b077b11263b2325ce6401581d335c130
Eric G <eric.gorr@twosixtech.com>
2023-04-12 20:40:27 -0400

    [main] tried to fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
+import { join } from 'path';
+import serveStatic from 'serve-static';
+import path from 'path';
+
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue()
+  ],
+
+  server: {
+    middleware: [
+      serveStatic(join(__dirname, 'node_modules', 'cesium', 'Build', 'Cesium'))
+    ]
+  }
 })
