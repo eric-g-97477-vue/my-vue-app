47c5dd72ff0fb2a4786669ff9f03a03c6b2a34f5
Eric G <eric.gorr@twosixtech.com>
2023-04-13 14:22:32 -0400

    [use-plugin] attempted to use the cesium plugin

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,14 @@ import { defineConfig } from 'vite'
 import { join } from 'path';
 import serveStatic from 'serve-static';
 import path from 'path';
+import cesium from 'vite-plugin-cesium';
 
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue()
+    vue(),
+    cesium()
   ],
-
-  server: {
-    middleware: [
-      serveStatic(join(__dirname, 'node_modules', 'cesium', 'Build', 'Cesium'))
-    ]
-  }
 })
